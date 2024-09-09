dbe7cf84542d9e2accd719eea1f9fc0d43eda2b5
valyaplotnikova <164873470+valyaplotnikova@users.noreply.github.com>
2024-09-09 15:22:32 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,8 @@
+from fake_math import divide as div_fake
+from true_math import divide as div_true
+
+
+print(div_fake(25, 0))
+print(div_fake(25, 5))
+print(div_true(14, 0))
+print(div_true(14, 2))
---
+++ true_math.py
@@ -0,0 +1,8 @@
+from math import inf
+
+
+def divide(first, second):
+    if second != 0:
+        return first / second
+    else:
+        return inf
