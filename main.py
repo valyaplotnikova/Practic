c946a9be768d0ccdf9bf48a8f5d93d086480a949
valyaplotnikova <164873470+valyaplotnikova@users.noreply.github.com>
2024-11-26 10:25:03 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+"1st program"
+print(9 ** 0.5 * 5)
+
+"2nd program"
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+"3rd program"
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(2 * (2 + 2) == 2 * 2 + 2)
+
+"4th program"
+st = '123.456'
+num = int(float(st) * 10) % 10
+print(num)
