1cb51e76b434771d9874f7078c24d8105134a729
valyaplotnikova <164873470+valyaplotnikova@users.noreply.github.com>
2024-12-23 14:09:41 +0300

    Add files via upload

---
+++ module_13_2.py
@@ -0,0 +1,24 @@
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+api = '7115803232:AAF67W6cAht1Gh_od5sg-CTN9un2ZABDqJ4'
+bot = Bot(token=api)
+dp = Dispatcher()
+
+# Этот хэндлер будет срабатывать на команду "/start"
+@dp.message(Command(commands=["start"]))
+async def start(message: Message):
+    await message.answer('Привет!\nЯ бот помогающий твоему здоровью.')
+
+
+# Этот хэндлер будет срабатывать на любые текстовые сообщения,
+# кроме команд "/start"
+@dp.message()
+async def all_massages(message: Message):
+    await message.answer('Введите команду /start, чтобы начать общение')
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
+
